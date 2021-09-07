6f91c0e103f0bd84e9e2876354edd2430f810816
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-07 10:43:02 +0100

    Create Rappel.java

---
+++ src/main/java/com/http/tp/entities/Rappel.java
@@ -0,0 +1,57 @@
+package com.http.tp.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.sun.istack.NotNull;
+
+@Entity
+public class Rappel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String titre,date;
+	@Column(nullable = true)
+	private String description;
+	@NotNull
+	private String type;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitre() {
+		return titre;
+	}
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
+	
+	
+	
+}
