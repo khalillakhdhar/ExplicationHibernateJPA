1a7a81b48030170f8724be3a52c527654de5d3a4
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-07 11:16:13 +0100

    Create Document.java

---
+++ src/main/java/com/http/tp/exercice/Document.java
@@ -0,0 +1,63 @@
+package com.http.tp.exercice;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.URL;
+
+@Entity
+public class Document {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@Column(nullable = true)
+	private String description;
+	private int iduser;
+	@NotBlank
+	private String titre;
+	@URL
+	@NotBlank
+	private String url;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getIduser() {
+		return iduser;
+	}
+	public void setIduser(int iduser) {
+		this.iduser = iduser;
+	}
+	public String getTitre() {
+		return titre;
+	}
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	
+	
+	
+	
+	
+	
+}
