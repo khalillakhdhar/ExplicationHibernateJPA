d174d3f91ae84a8f59a9ba5def73176e8b243050
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-07 10:16:52 +0100

    Initial commit

---
+++ src/main/java/com/http/tp/ExplicationHibernateJpaApplication.java
@@ -0,0 +1,13 @@
+package com.http.tp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplicationHibernateJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplicationHibernateJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/http/tp/ExplicationHibernateJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.http.tp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplicationHibernateJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
